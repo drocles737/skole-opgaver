343a0b44416ab9ecded08ab7c20eb6e72e10e67b
andreas nielsen <xkiller747@hotmail.com>
2018-06-11 12:27:56 +0200

    jeg kan lide kage

---
+++ motorcykel1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace motorcykel1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            motorcykel yamaha = new motorcykel(true, 1000, 2, "jesus bike");
+
+            Console.WriteLine(yamaha);
+           
+
+        }
+    }
+}
---
+++ motorcykel1/motorcykel.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace motorcykel1
+{
+    class motorcykel
+    {
+        public motorcykel()
+        { }
+
+        public Boolean startet;
+
+        public int Rpm { get; set; }
+
+        public int gear { get; set; }
+
+        public string name { get; set; }
+
+        public double speed { get; set; }
+
+
+        public Boolean nystartet
+        {
+            get { return startet; }
+            set
+            {
+                if (startet == true)
+                {
+
+                    Rpm = 1000;
+
+
+                }
+
+
+            }
+
+        }
+
+        public int mitgear
+        {
+            get { return gear; }
+            set { gear = value; }
+
+        }
+        public int mitrpm
+        {
+            get { return Rpm; }
+            set
+            {
+                if (startet == true && Rpm < 8000)                           //(value >= 1001 || value <= 8000)
+                {
+                    Rpm = value;
+
+                }
+                else if (value < 1000 && Rpm < 1000)
+                {
+                    startet = false;
+
+                }
+                
+
+            }
+
+        }
+        public void start()  //skal returnere værdien 
+        {
+            if (!startet)
+            {
+                startet = true;
+                Rpm = 1000;
+                gear = 0;
+
+            }
+            
+        }
+        public void stop()
+        {
+            if (startet == true)
+            {
+                startet = false;
+                Rpm = 0;
+                gear = 0;
+
+
+            }
+
+        }
+        
+        public double getspeed()
+        {
+            speed = Rpm * gear / 200;
+            return speed;
+        }
+        //public double hastighed { get; set; }
+        public void shiftgearup()
+        {
+            if (gear < 5 && startet == true)
+            {
+                gear++;
+                
+            }
+            
+        }
+        public shiftgeardown(int g)  //bruges til at sætte gearet ned hvis det er for højt
+        {
+            if (g < gear && g > 0)
+            {
+                gear = g;
+                
+            }
+
+        }
+        public int getgear()
+        {
+            return gear;
+
+        }
+        
+        
+
+
+        public motorcykel(Boolean isstarted, int Rmp1, int gear1, string name1, double gethastighed)
+        {
+            startet = isstarted;
+            Rpm = Rmp1;
+            gear = gear1;
+            name = name1;
+            // = gethastighed;
+
+
+        }
+        public virtual string fuldtmotorcykel()
+        {
+            return startet + " " + Rpm + " " + gear + " " + name + " " + speed;
+
+
+        }
+
+        //gear =gear++;
+        
+
+
+
+
+    }
+}
